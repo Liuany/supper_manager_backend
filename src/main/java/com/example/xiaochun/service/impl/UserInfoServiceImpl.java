5736e441e19e496b82f3036956ac32e41e29d9e6
wen.liu1 <wen.liu1@chinacreator.com>
2021-04-06 18:00:48 +0800

    修改

--- src/main/java/com/example/xiaochun/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/example/xiaochun/service/impl/UserInfoServiceImpl.java
@@ -5,21 +5,26 @@ import com.example.xiaochun.mapper.UserInfoMapper;
 import com.example.xiaochun.model.dto.RegisterDTO;
 import com.example.xiaochun.model.entity.UserInfo;
 import com.example.xiaochun.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
+import javax.annotation.Resource;
 
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo>
         implements UserInfoService {
 
+    @Resource
+    private UserInfoMapper userInfoMapper;
+
     @Override
     public UserInfo executeRegister(RegisterDTO dto) {
-        UserInfo registerUser = UserInfo.builder()
+        UserInfo registeredUser = UserInfo.builder()
                 .userName(dto.getUserName())
                 .nikeName(dto.getNickName())
                 .passWord(dto.getPassWord())
                 .build();
-        return null;
+        userInfoMapper.insert(registeredUser);
+        return registeredUser;
     }
 }
